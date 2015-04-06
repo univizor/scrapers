fd03f00dc54f0ba6303b679f33681989c8dd3811
Ziga Stopinsek <sigi.kajzer@gmail.com>
2015-04-06 22:06:10 +0200

    settings in environ

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+
+# export DB_HOST='localhost'; export DB_USER='dev'; export DB_PASS='dev'; export DB_NAME='univizor'
+
+DB_HOST = os.environ['DB_HOST']
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
+DB_DATABASE = os.environ['DB_NAME']
+
+DOC_PATH = './docs'
