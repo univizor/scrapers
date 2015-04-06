e222ae6a7798c0c5fa60e8b1e2d4798356906ad6
Ziga Stopinsek <sigi.kajzer@gmail.com>
2015-04-06 22:33:12 +0200

    bugfix in settings

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['DB_PASS']
 DB_DATABASE = os.environ['DB_NAME']
 
-DOC_PATH = os.environ['DOC_PATH'] or '/mnt/univizor/download/'
+DOC_PATH = os.environ.get('DOC_PATH') or '/mnt/univizor/download/'
