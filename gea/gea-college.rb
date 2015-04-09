d5b3170f5427abe570cb5e68b489459b8283f287
univizoruser <andraz.tori+univizor@gmail.com>
2015-04-09 20:39:42 +0000

    multiple url_roots (for MSc); option to redownload PDFs

--- gea/gea-college.rb
+++ gea/gea-college.rb
@@ -6,69 +6,73 @@ require 'rest-client'
 require_relative '../../converter/ruby/database.rb'
 
 OUTPUT_DIR = '/mnt/univizor/download/gea/'
+REDOWNLOAD = false
 
-url_root = "http://gea.gea-college.si/diplome/diplomska_dela_vsp_2009.aspx?studij=1"
-result = open(url_root)
-list_doc = Nokogiri.XML(result)
-list_doc.encoding = 'utf-8'
-form = list_doc.search('form')[0]
+url_roots = ["http://gea.gea-college.si/diplome/diplomska_dela_vsp_2009.aspx?studij=1", "http://gea.gea-college.si/diplome/diplomska_dela_vsp_2009.aspx?studij=4"]
+url_roots.each do |url_root|
+  result = open(url_root)
+  list_doc = Nokogiri.XML(result)
+  list_doc.encoding = 'utf-8'
+  form = list_doc.search('form')[0]
 
-params = {"__EVENTTARGET" => "", "__EVENTARGUMENT" => "", "__LASTFOCUS" => "", "Textbox1" => ""}
+  params = {"__EVENTTARGET" => "", "__EVENTARGUMENT" => "", "__LASTFOCUS" => "", "Textbox1" => ""}
 
-form.children.each do |child|
-  m = child.to_s.match(/input type="hidden".*name="(.*?)".*value="(.*?)"/)
-  if m
-    params[m[1]] = m[2]
-  end
-end
-
-list_doc.css('input').each do |node|
-  params2 = params.dup
-  filename = ""
-  if node.attributes['name'].content =~ /^DataList1\$ctl/
-    key_x = node.attributes['name'].content + ".x"
-    key_y = node.attributes['name'].content + ".y"
-    params2[key_x] = "1"
-    params2[key_y] = "1"
-    filename = node.attributes['name'].content.match(/\$(.*)\$/)[1] + ".pdf"
-  else
-    next
+  form.children.each do |child|
+    m = child.to_s.match(/input type="hidden".*name="(.*?)".*value="(.*?)"/)
+    if m
+      params[m[1]] = m[2]
+    end
   end
 
-  doc = RestClient.post url_root, params2
+  list_doc.css('input').each do |node|
+    params2 = params.dup
+    filename = ""
+    if node.attributes['name'].content =~ /^DataList1\$ctl/
+      key_x = node.attributes['name'].content + ".x"
+      key_y = node.attributes['name'].content + ".y"
+      params2[key_x] = "1"
+      params2[key_y] = "1"
+      filename = node.attributes['name'].content.match(/\$(.*)\$/)[1] + ".pdf"
+    else
+      next
+    end
 
-  url = "geacollege-#{filename}"
+    url = "geacollege-#{filename}"
 
-  # insert into db (or update)
-  diploma = Diploma.find_by(url: url)
-  if !diploma
+    # insert into db (or skip)
+    diploma = Diploma.find_by(url: url)
+    if diploma && !REDOWNLOAD
+      puts "skipping #{url}"
+      next
+    end
     diploma = Diploma.new
     diploma.filename =''
     diploma.save
-  end
-  diploma.url = url
-  diploma.filename = OUTPUT_DIR + diploma.id.to_s + ".pdf"
-  diploma.naslov = ''
-  diploma.fakulteta = ''
-  diploma.leto = ''
-  diploma.data = ''
-  diploma.avtor = ''
-  diploma.save
- 
-  # change filename
-  id_filename = OUTPUT_DIR + "#{diploma.id}.pdf"
+    diploma.url = url
+    diploma.filename = OUTPUT_DIR + diploma.id.to_s + ".pdf"
+    diploma.naslov = ''
+    diploma.fakulteta = ''
+    diploma.leto = ''
+    diploma.data = ''
+    diploma.avtor = ''
+    diploma.save
+   
+    # change filename
+    id_filename = OUTPUT_DIR + "#{diploma.id}.pdf"
 
-  # save to disk
-  if File.exist?(id_filename)
-    puts "SKIP writing #{id_filename}"
-  else
-    puts "writing #{id_filename}"
-    File.write(id_filename, doc)
+    doc = RestClient.post url_root, params2
+
+    # save to disk
+    if File.exist?(id_filename)
+      puts "SKIP writing #{id_filename}"
+    else
+      puts "writing #{id_filename}"
+      File.write(id_filename, doc)
+    end
+    
+    sleep 1
   end
-  
-  sleep 1
 end
-
 """
 POST /diplome/diplomska_dela_vsp_2009.aspx?studij=1 HTTP/1.1
 Host: gea.gea-college.si
